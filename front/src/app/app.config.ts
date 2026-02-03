434ff621fd03bcd8afc70a2d054acc11c0f3da1e
@dissoares <diegosoaresyt@gmail.com>
2026-02-03 13:12:42 -0300

    configurando app

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,12 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+  ApplicationConfig,
+} from '@angular/core';
+import {
+  withEnabledBlockingInitialNavigation,
+  withRouterConfig,
+  provideRouter,
+} from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { authInterceptorFn } from './core/auth/auth-interceptor';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { PaginacaoCustomizada } from './core/shared';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(
+      routes,
+      withEnabledBlockingInitialNavigation(),
+      withRouterConfig({ onSameUrlNavigation: 'reload' }),
+    ),
+    provideHttpClient(withInterceptors([authInterceptorFn])),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      timeOut: 1500,
+      closeButton: true,
+      progressBar: true,
+    }),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    importProvidersFrom(MatDatepickerModule, MatNativeDateModule),
+    { provide: MatPaginatorIntl, useClass: PaginacaoCustomizada },
+  ],
 };
