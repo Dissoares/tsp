1f3058ecbda2f502664febf459681b0fc70e9f5c
@dissoares <diegosoaresyt@gmail.com>
2026-02-03 13:32:28 -0300

    configurando rota

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,28 @@
+import { AuthGuard } from './core/guards/auth.guard';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    canActivate: [AuthGuard],
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/conteudo/conteudo.component').then((c) => c.ConteudoComponent),
+    children: [
+      {
+        path: '**',
+        redirectTo: '/auth',
+        pathMatch: 'full',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: '/auth',
+    pathMatch: 'full',
+  },
+];
