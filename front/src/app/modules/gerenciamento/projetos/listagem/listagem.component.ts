3c0be160879572cc0cca29f6e0101f78b1c64a30
@dissoares <diegosoaresyt@gmail.com>
2026-02-04 09:17:00 -0300

    atualizando arquitetura

--- front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
+++ front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
@@ -1,3 +1,4 @@
+import { FiltrosComponent } from './filtros/filtros.component';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,7 +7,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   templateUrl: './listagem.component.html',
   styleUrls: ['./listagem.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, FiltrosComponent],
 })
 export class ListagemComponent implements OnInit {
   constructor() {}
