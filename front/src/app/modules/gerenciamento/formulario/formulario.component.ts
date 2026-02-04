0c0b2ba8921993a4bce3836e9a40bd80c793b864
@dissoares <diegosoaresyt@gmail.com>
2026-02-04 11:31:26 -0300

    adicionando formulario solicitação

--- front/src/app/modules/gerenciamento/formulario/formulario.component.ts
+++ front/src/app/modules/gerenciamento/formulario/formulario.component.ts
@@ -1,15 +1,53 @@
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-formulario',
   standalone: true,
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.scss'],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+  ],
 })
 export class FormularioComponent implements OnInit {
-  constructor() {}
+  public formulario!: FormGroup;
 
-  public ngOnInit(): void {}
+  constructor(public fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.criarFormulario();
+  }
+
+  public criarFormulario(): void {
+    this.formulario = this.fb.group({
+      tituloProjeto: [null],
+      descricaoProjeto: [null],
+      responsavelProjeto: [null],
+      motivacaoProjeto: [null],
+      objetivosEspecificosProjeto: [null],
+      objetivosEstrategicosSesa: [null],
+      premissas: [null],
+      riscos: [null],
+      restricoes: [null],
+      aprovacao: [null],
+      projetoViavel: [null],
+      estimativaTempo: [null],
+      estimativaCusto: [null],
+    });
+  }
 }
