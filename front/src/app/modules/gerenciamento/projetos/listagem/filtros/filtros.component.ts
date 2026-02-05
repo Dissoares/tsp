4a552d8fdc38c27cdb3a485476cdee044100cfce
@dissoares <diegosoaresyt@gmail.com>
2026-02-05 09:31:26 -0300

    adicionando filtros e listagem

--- front/src/app/modules/gerenciamento/projetos/listagem/filtros/filtros.component.ts
+++ front/src/app/modules/gerenciamento/projetos/listagem/filtros/filtros.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormsModule, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filtros',
   standalone: true,
   templateUrl: './filtros.component.html',
   styleUrls: ['./filtros.component.scss'],
-  imports: [CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class FiltrosComponent implements OnInit {
-  constructor() {}
+  public formulario!: FormGroup;
 
-  public ngOnInit(): void {}
+  private readonly router = inject(Router);
+
+  constructor(public fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.criarFormulario();
+  }
+
+  public criarFormulario(): void {
+    this.formulario = this.fb.group({
+      periodoEnvio: [null],
+      tituloProjeto: [null],
+      responsavelProjeto: [null],
+      statusProjeto: [null],
+      prioridade: [null],
+      estimativaTempo: [null],
+    });
+  }
+
+  public novoProjeto() {
+    this.router.navigate(['']);
+  }
 }
--- front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
+++ front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
@@ -1,4 +1,12 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FiltrosComponent } from './filtros/filtros.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +15,20 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   templateUrl: './listagem.component.html',
   styleUrls: ['./listagem.component.scss'],
-  imports: [CommonModule, FiltrosComponent],
+  imports: [
+    CommonModule,
+    FiltrosComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class ListagemComponent implements OnInit {
   constructor() {}
