e3a18bf0b9cd176f6ba054a04a05c63a318a029f
@dissoares <diegosoaresyt@gmail.com>
2026-02-05 10:18:31 -0300

    atulizando listagem e filtros

--- front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
+++ front/src/app/modules/gerenciamento/projetos/listagem/listagem.component.ts
@@ -1,29 +1,33 @@
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FiltrosComponent } from './filtros/filtros.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SolicitacaoProjeto } from '../../../../core/models';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-listagem',
   standalone: true,
   templateUrl: './listagem.component.html',
   styleUrls: ['./listagem.component.scss'],
   imports: [
-    CommonModule,
     FiltrosComponent,
     ReactiveFormsModule,
     MatFormFieldModule,
+    MatPaginatorModule,
     MatToolbarModule,
     MatButtonModule,
     MatSelectModule,
     MatInputModule,
+    MatTableModule,
     MatCardModule,
     MatIconModule,
     CommonModule,
@@ -31,7 +35,50 @@ import { CommonModule } from '@angular/common';
   ],
 })
 export class ListagemComponent implements OnInit {
-  constructor() {}
+  public formulario!: FormGroup;
+  @ViewChild(MatPaginator) private paginator!: MatPaginator;
 
-  public ngOnInit(): void {}
+  public dadosTabela = new MatTableDataSource<SolicitacaoProjeto>([]);
+  public colunasTabela: Array<string> = [
+    'periodoEnvio',
+    'tituloProjeto',
+    'responsavelProjeto',
+    'statusProjeto',
+    'prioridade',
+    'estimativaTempo',
+    'acoes',
+  ];
+
+  private readonly toastr = inject(ToastrService);
+  constructor(public fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.criarFormulario();
+  }
+
+  public criarFormulario(): void {
+    this.formulario = this.fb.group({
+      periodoEnvio: [null],
+      tituloProjeto: [null],
+      responsavelProjeto: [null],
+      statusProjeto: [null],
+      prioridade: [null],
+      estimativaTempo: [null],
+    });
+  }
+
+  public editar(solicitacao: SolicitacaoProjeto): void {
+    if (this.formulario.valid) {
+      this.toastr.info('Só é possivel uma edição por vez', 'Aviso!');
+      return;
+    }
+    this.formulario.patchValue(solicitacao);
+
+    const index = this.dadosTabela.data.indexOf(solicitacao);
+    if (index > -1) {
+      this.dadosTabela.data.splice(index, 1);
+    }
+  }
+
+  public visualizar(solicitacao: SolicitacaoProjeto): void {}
 }
